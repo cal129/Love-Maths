574d50c7e944dee9e825090c38c8952d327b1e68
Callum <callum@mac.home>
2025-12-18 14:38:17 +0000

    update all

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,8 +35,8 @@ function runGame(gameType) {
     document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
+    let num1 = Math.floor(Math.random() * 15) + 1;
+    let num2 = Math.floor(Math.random() * 15) + 1;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
